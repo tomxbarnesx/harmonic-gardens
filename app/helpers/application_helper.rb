f9f5bfdabdcd81f75383c7700689461c434d1153
Tom Barnes <barnes.thomas.c@gmail.com>
2018-10-16 22:33:25 -0400

    added role helper and if statements in schedule.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def has_role?(role)
+        if current_user.user_access == role
+            return true
+        end
+    end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def abbrv_name
+      "#{first_name.slice(0,1)}. #{last_name}"
+  end
 end
