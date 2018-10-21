ec08182cb715d718cc7fe2f6bee756f2687a5819
Tom Barnes <barnes.thomas.c@gmail.com>
2018-10-21 15:06:33 -0400

    more changes to tables

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,5 +1,6 @@
 $(document).ready( function () {
     $('#datatable').DataTable( {
+        "pageLength": 25,
         columnDefs: [
             { orderable: false, targets: -1 },
             { orderable: false, targets: -2 },
