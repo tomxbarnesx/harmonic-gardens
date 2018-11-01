99be480ba28e7c12cca7719e1fd4f3a3f49d194a
Tom Barnes <barnes.thomas.c@gmail.com>
2018-11-01 18:38:02 -0400

    added hide and show features

---
+++ app/assets/javascripts/invoice_commands.js
@@ -0,0 +1,14 @@
+$(document).ready( function () {
+
+    $(".hide-button").on("click", function() {
+        if ($(this).data("open") == undefined || $(this).data("open") == "false") {
+            $(this).parent().css({"overflow": "hidden", "height": "5.5em"});
+            $(this).html("&#x21a7;");
+            $(this).data("open", "true");
+        } else {
+            $(this).parent().css({"overflow": "visible", "height": "100%"});
+            $(this).html("&#x21a5;");
+            $(this).data("open", "false");
+        }
+    });
+});
