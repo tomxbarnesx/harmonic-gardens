63a6f7640c37e7056a1e140f8146b36ce47a4bd3
Tom Barnes <barnes.thomas.c@gmail.com>
2018-11-08 13:15:40 -0500

    updated shift views for mobile

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    var notice = document.getElementById("notice");
+    var alert = document.getElementById("alert");
+    if (notice.innerHTML == ""){
+      notice.style.display = "none";
+    }
+    if (alert.innerHTML == "") {
+      alert.style.display = "none";
+    };
+  })
