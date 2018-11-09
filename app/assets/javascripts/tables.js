744ac9ea2806687fd95e9f0c5ab508dd0be177c4
Tom Barnes <barnes.thomas.c@gmail.com>
2018-11-09 12:41:25 -0500

    updated table styling

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,6 +1,7 @@
 $(document).ready( function () {
     $('#datatable').DataTable( {
         "pageLength": 25,
+        "bLengthChange": false,
         columnDefs: [
             { orderable: false, targets: -1 },
             { orderable: false, targets: -2 },
