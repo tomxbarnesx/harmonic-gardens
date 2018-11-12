49e276649cf8e9e44a3a32b1df72096bbfa14fdf
Tom Barnes <barnes.thomas.c@gmail.com>
2018-11-11 22:52:28 -0500

    added balance to the invoice styling default close features for the invoices

--- app/assets/javascripts/invoice_commands.js
+++ app/assets/javascripts/invoice_commands.js
@@ -1,15 +1,16 @@
 $(document).ready( function () {
+    $(".invoice-clip-container").hide()
     $(document).on("click", ".hide-button", function() {
-        if ($(this).data("open") == undefined || $(this).data("open") == "false") {
-            $(this).nextAll(".invoice-clip-container").hide();
-            // $(this).parent().css({"overflow": "hidden", "height": "5.5em"});
-            $(this).html("&#x21a7;");
-            $(this).data("open", "true");
-        } else {
+        if ($(this).data("closed") == undefined || $(this).data("closed") == "true") {
             $(this).nextAll(".invoice-clip-container").show();
+            // $(this).parent().css({"overflow": "hidden", "height": "5.5em"});
+            $(this).html("<i class='fas fa-chevron-circle-down'></i>");
+            $(this).data("closed", "false");
+        } else {
+            $(this).nextAll(".invoice-clip-container").hide();
             // $(this).parent().css({"overflow": "visible", "height": "100%"});
-            $(this).html("&#x21a5;");
-            $(this).data("open", "false");
+            $(this).html("<i class='fas fa-chevron-circle-up'></i>");
+            $(this).data("closed", "true");
         }
     });
 });
